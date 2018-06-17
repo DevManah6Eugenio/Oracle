577d2e6a3034ba82e0ecd973777430ff7a24793d
Manasses Java <manasses.java@gmail.com>
2018-06-17 16:08:43 -0300

    Create 04-sequence_endereco.sql

---
+++ 04-sequence_endereco.sql
@@ -0,0 +1,3 @@
+/*criação do sequence para a tabela de endereço, para gerar valores 
+sequencias para servi como identificação da tabela endereço*/
+CREATE SEQUENCE S_ID_ENDERECO INCREMENT BY 1 START WITH 1 MINVALUE 1;
