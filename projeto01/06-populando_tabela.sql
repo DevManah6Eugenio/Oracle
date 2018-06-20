a0ecb312aebc361eb84800f2ddcd461ca5eedc22
Manasses Java <manasses.java@gmail.com>
2018-06-19 21:58:55 -0300

    Update 06-populando_tabela.sql

--- projeto01/06-populando_tabela.sql
+++ projeto01/06-populando_tabela.sql
@@ -6,3 +6,7 @@ INSERT INTO CONTA(NUMERO, DONO, LIMITE, SALDO) VALUES ('12345', 'CLEITON', '10',
 INSERT INTO CONTA(NUMERO, DONO, LIMITE, SALDO) VALUES ('54321', 'Manasses', '10', '110');
 INSERT INTO CONTA(NUMERO, DONO, LIMITE, SALDO) VALUES ('121212', 'Erica', '10', '120');
 INSERT INTO CONTA(NUMERO, DONO, LIMITE, SALDO) VALUES ('112233', 'Miqueias', '10', '90');
+
+/*inserindo dados na tabela endereço*/
+INSERT INTO ENDERECO(RUA, NUMERO, COMPLEMENTO) VALUES ('RUA FULANA DE TAL', '300', 'CASA');
+insert into endereco(rua, numero, complemento) values ('rua fulana de tal', '300', 'casa');
