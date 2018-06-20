39d7e481a675314acfce68b0cccfb8d047323be0
Manasses Java <manasses.java@gmail.com>
2018-06-20 17:53:40 -0300

    Update 01-tabela_compras.sql

--- projeto02/01-tabela_compras.sql
+++ projeto02/01-tabela_compras.sql
@@ -1,7 +1,11 @@
 /*TABELA COMPRAS*/
 create table compras (
+    id number primary key,
     valor number,
     data date,
     observacoes varchar2(30),
     recebido char check (recebido in (0,1))
 );
+
+/*sequende*/
+create sequence id_seq;
