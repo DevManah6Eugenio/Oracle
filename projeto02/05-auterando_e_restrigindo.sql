f55de1cfb27e84d799945f8f8d8897deda0b9b4e
Manasses Java <manasses.java@gmail.com>
2018-06-21 18:20:00 -0300

    Update and rename 07-auterando_e_restrigindo.sql to 05-auterando_e_restrigindo.sql

--- projeto02/05-auterando_e_restrigindo.sql
+++ projeto02/05-auterando_e_restrigindo.sql
@@ -3,3 +3,6 @@ ALTER TABLE COMPRAS MODIFY (OBSERVACOES VARCHAR2(30) NOT NULL);
 
 /*compo recebido por padrão deve receber o valor de falso*/
 ALTER TABLE COMPRAS MODIFY (RECEBIDO CHAR DEFAULT '0' CHECK (RECEBIDO IN (0,1)));
+
+/*nova coluna com a informação do tipo de pagamento onde os valores permitidos são cartão, boleto e dinheiro*/
+ALTER TABLE COMPRAS ADD (FORMA_PAGTO VARCHAR2(15) CHECK (FORMA_PAGTO IN ('CARTAO', 'BOLETO', 'DINHEIRO')));
