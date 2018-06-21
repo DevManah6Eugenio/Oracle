dd8cb623f097291ade59d2278d7322b9b7e3c445
Manasses Java <manasses.java@gmail.com>
2018-06-21 07:33:00 -0300

    Create 07-atividade_cleiton.sql

---
+++ projeto01/07-atividade_cleiton.sql
@@ -0,0 +1,15 @@
+/*
+ATIVIDADE
+
+inserir em suas tabelas respectivamente:
+
+  5 registros de Endereço;
+  5 registros de Pessoa;
+  5 registros de Conta;
+  
+realizar duas consultas simples e uma complexa(SELECT) que retorne as seguintes informações:
+
+  1 - Quero todas as pessoas que residem em apartamentos.
+  2 - Quero todas as pessoas que tenha saldoda conta MAIOR que R$100,00
+  3 - Quero os endereços das pessoas que não tem saldo em conta e residem em casa.
+*/
