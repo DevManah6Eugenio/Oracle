7bf8c163485d2a412fa18b30d57cd4caf06abdae
Manasses Java <manasses.java@gmail.com>
2018-06-21 18:54:37 -0300

    Update 05-auterando_e_restrigindo.sql

--- projeto02/05-auterando_e_restrigindo.sql
+++ projeto02/05-auterando_e_restrigindo.sql
@@ -6,3 +6,12 @@ ALTER TABLE COMPRAS MODIFY (RECEBIDO CHAR DEFAULT '0' CHECK (RECEBIDO IN (0,1)))
 
 /*nova coluna com a informação do tipo de pagamento onde os valores permitidos são cartão, boleto e dinheiro*/
 ALTER TABLE COMPRAS ADD (FORMA_PAGTO VARCHAR2(15) CHECK (FORMA_PAGTO IN ('CARTAO', 'BOLETO', 'DINHEIRO')));
+
+/*modificar todos as formas de pagamento nulas pra tipo de pagamento boleto*/
+UPDATE compras SET forma_pagto = 'BOLETO' WHERE forma_pagto IS NULL;
+
+/*o campo observação não pode ser nullo*/
+UPDATE COMPRAS SET OBSERVACOES = '(VAZIO)' WHERE OBSERVACOES IS NULL;
+
+/*não permitir valores nullos em observações*/
+ALTER TABLE COMPRAS MODIFY (OBSERVACOES VARCHAR2(30) NOT NULL);
