7bcff0f33a9b4e744494254c86270bb75a689229
Manasses Java <manasses.java@gmail.com>
2018-06-24 15:59:59 -0300

    Update 07-atividade_cleiton.sql

--- projeto01/07-atividade_cleiton.sql
+++ projeto01/07-atividade_cleiton.sql
@@ -10,7 +10,7 @@ inserir em suas tabelas respectivamente:
 realizar duas consultas simples e uma complexa(SELECT) que retorne as seguintes informações:
 
   1 - Quero todas as pessoas que residem em apartamentos.
-  2 - Quero todas as pessoas que tenha saldoda conta MAIOR que R$100,00
+  2 - Quero todas as pessoas que tenha saldo da conta MAIOR que R$100,00
   3 - Quero os endereços das pessoas que não tem saldo em conta e residem em casa.
 */
 
@@ -22,7 +22,30 @@ insert into endereco (id_endereco, rua, numero, complemento) values ( s_id_ender
 insert into endereco (id_endereco, rua, numero, complemento) values ( s_id_endereco.nextval, 'rua Imperial', '47', 'casa');
 
 /*5 registros de Conta*/
-insert into conta (numero, dono, limite, saldo, saldo) values ();
+insert into conta (numero, dono, limite, saldo) values ('123', null, null, '100');
+insert into conta (numero, dono, limite, saldo) values ('321', null, null, '50');
+insert into conta (numero, dono, limite, saldo) values ('456', null, null, '150');
+insert into conta (numero, dono, limite, saldo) values ('789', null, null, '200');
+insert into conta (numero, dono, limite, saldo) values ('987', null, null, '0');
 
 /*5 registros de Pessoa*/
-insert into endereco (nome, idade, sexo, id_endereco, numero_conta) values ('cleiton', 32, 'm', 1, );
+insert into pessoa (nome, idade, sexo, cpf, id_endereco, numero_conta) values ('cleiton', '32', 'M', '12345678900', '1', '123');
+insert into pessoa (nome, idade, sexo, cpf, id_endereco, numero_conta) values ('manasses', '23', 'M', '09876543211', '2', '321');
+insert into pessoa (nome, idade, sexo, cpf, id_endereco, numero_conta) values ('erica', '24', 'F', '12312312312', '3', '456');
+insert into pessoa (nome, idade, sexo, cpf, id_endereco, numero_conta) values ('rebeca', '20', 'F', '32132132132', '4', '789');
+insert into pessoa (nome, idade, sexo, cpf, id_endereco, numero_conta) values ('miqueias', '22', 'M', '24242424242', '5', '987');
+
+/* 1 - Quero todas as pessoas que residem em apartamentos.*/
+select nome from pessoa inner join endereco on (pessoa.id_endereco = endereco.id_endereco) where complemento = 'apar';
+
+/* 2 - Quero todas as pessoas que tenha saldo da conta MAIOR que R$100,00*/
+select nome from pessoa inner join conta on (pessoa.numero_conta = conta.numero) where saldo > '100';
+
+/* 3 - Quero os endereços das pessoas que não tem saldo em conta e residem em casa.*/
+select endereco.numero, rua, complemento from pessoa 
+    inner join endereco on 
+    (pessoa.id_endereco = endereco.id_endereco) 
+    inner join conta on 
+    (pessoa.numero_conta = conta.numero)
+    where (saldo is null or saldo = '0')
+    and complemento = 'casa';
