ad80eb5dfb1a8ba46a6ec875f27140ab75087e4b
Manasses Java <manasses.java@gmail.com>
2018-06-24 13:43:11 -0300

    Create 08-.sql

---
+++ projeto02/08-.sql
@@ -0,0 +1,9 @@
+/*adicionar a tabela compras o compo comprador_id*/
+alter table compras add (comprador_id number);
+
+/*quando o valor de identificação da compra for maior que 20 o valor de comprador_id de ser 2
+e se for menor 1*/
+update compras set comprador_id = 1 where id < 20;
+update compras set comprador_id = 2 where id > 20;
+
+alter table compras add forign key (comprador_id) references compradores(id);
