89b24b911fbc05c021b3caebfa741f83a42c1072
Manasses Java <manasses.java@gmail.com>
2018-06-24 13:44:18 -0300

    Update 07-tabela_compradores.sql

--- projeto02/07-tabela_compradores.sql
+++ projeto02/07-tabela_compradores.sql
@@ -1,3 +1,4 @@
+/*tabela compradores*/
 create table compradores (
 id number primary key,
 nome varchar2(30) not null,
@@ -5,7 +6,11 @@ endereco varchar2(30) not null,
 telefone varchar2(20) not null
 );
 
+/*sequence*/
 create sequence id_compradores_seq;
 
 insert into compradores (id, nome, endereco, telefone) 
   values (id_compradores_seq.nextval, 'manasses', 'Rua fulana de tal', '(81) 1234567890');
+
+insert into compradores (id, nome, endereco, telefone) 
+  values (id_compradores_seq.nextval, 'rafael', 'Rua fulana de tal', '(81) 0987654321');
