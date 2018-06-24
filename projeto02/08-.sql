3b6c23ddc94cd85b2899926805ddac8f0f47b7d3
Manasses Java <manasses.java@gmail.com>
2018-06-24 13:56:21 -0300

    Update 08-.sql

--- projeto02/08-.sql
+++ projeto02/08-.sql
@@ -6,4 +6,4 @@ e se for menor 1*/
 update compras set comprador_id = 1 where id < 20;
 update compras set comprador_id = 2 where id > 20;
 
-alter table compras add forign key (comprador_id) references compradores(id);
+alter table compras add foreign key (comprador_id) references compradores(id);
