c6d6001074387d4ea7afbadac5d4c4e8eb6e7e46
Manasses Java <manasses.java@gmail.com>
2018-06-28 17:46:28 -0300

    Update 08-select.sql

--- projeto03/08-select.sql
+++ projeto03/08-select.sql
@@ -11,5 +11,40 @@ select a.nome from aluno a where not exists ( select m.id from matricula m
 
 -------------------------------------------------------------------------------------------------------------
 /*Exiba a média das notas por curso.*/
-select c.nome from curso c
-    join nota;
+select c.nome, avg(n.nota) as media from nota n
+    join resposta r on r.id = n.resposta_id
+    join exercicio e on e.id = r.exercicio_id
+    join secao s on s.id = e.secao_id
+    join curso c on c.id = s.curso_id
+group by c.nome;
+
+/*Selecione o curso e as médias de notas, levando em conta somente alunos que tenham "Silva" ou "Santos" no sobrenome.*/
+select c.nome, avg(n.nota) as media from nota n
+    join resposta r on r.id = n.resposta_id
+    join exercicio e on e.id = r.exercicio_id
+    join secao s on s.id = e.secao_id
+    join curso c on c.id = s.curso_id 
+    join aluno a on a.id = r.aluno_id
+    where a.nome like '%Silva%' or
+    a.nome like '%Santos%'
+group by c.nome;
+
+/*Conte a quantidade de respostas por exercício*/
+select e.pergunta ,count(r.id) from exercicio e
+    join resposta r on (r.exercicio_id = e.id)
+    group by e.pergunta; 
+    
+/*Pegue a resposta do exercício anterior, e ordene por número de respostas, em ordem decrescente.*/
+select e.pergunta ,count(r.id) from exercicio e
+    join resposta r on (r.exercicio_id = e.id)
+    group by e.pergunta order by count(r.id) desc; 
+    
+/*Podemos agrupar por mais de um campo de uma só vez. Por exemplo, se quisermos a média de notas por aluno por curso*/
+select a.nome, c.nome, avg(n.nota) as media from nota n
+    join resposta r on r.id = n.resposta_id
+    join exercicio e on e.id = r.exercicio_id
+    join secao s on s.id = e.secao_id
+    join curso c on c.id = s.curso_id
+    join aluno a on a.id = r.aluno_id
+group by a.nome, c.nome;
+
