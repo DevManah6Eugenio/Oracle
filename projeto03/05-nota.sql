338a3457cd15a36141cc6b39b6076deae20e7718
Manasses Java <manasses.java@gmail.com>
2018-06-27 22:48:26 -0300

    Create 05-nota.sql

---
+++ projeto03/05-nota.sql
@@ -0,0 +1,33 @@
+CREATE TABLE nota (
+  id number primary key,
+  resposta_id number DEFAULT NULL,
+  nota number DEFAULT NULL
+);
+
+INSERT INTO nota (id, resposta_id, nota) VALUES (1, 1, 8);
+INSERT INTO nota (id, resposta_id, nota) VALUES (2, 2, 0);
+INSERT INTO nota (id, resposta_id, nota) VALUES (3, 3, 7);
+INSERT INTO nota (id, resposta_id, nota) VALUES (4, 4, 6);
+INSERT INTO nota (id, resposta_id, nota) VALUES (5, 5, 9);
+INSERT INTO nota (id, resposta_id, nota) VALUES (6, 6, 10);
+INSERT INTO nota (id, resposta_id, nota) VALUES (7, 7, 4);
+INSERT INTO nota (id, resposta_id, nota) VALUES (8, 8, 4);
+INSERT INTO nota (id, resposta_id, nota) VALUES (9, 9, 7);
+INSERT INTO nota (id, resposta_id, nota) VALUES (10, 10, 8);
+INSERT INTO nota (id, resposta_id, nota) VALUES (11, 11, 6);
+INSERT INTO nota (id, resposta_id, nota) VALUES (12, 12, 7);
+INSERT INTO nota (id, resposta_id, nota) VALUES (13, 13, 4);
+INSERT INTO nota (id, resposta_id, nota) VALUES (14, 14, 9);
+INSERT INTO nota (id, resposta_id, nota) VALUES (15, 15, 3);
+INSERT INTO nota (id, resposta_id, nota) VALUES (16, 16, 5);
+INSERT INTO nota (id, resposta_id, nota) VALUES (17, 17, 5);
+INSERT INTO nota (id, resposta_id, nota) VALUES (18, 18, 5);
+INSERT INTO nota (id, resposta_id, nota) VALUES (19, 19, 6);
+INSERT INTO nota (id, resposta_id, nota) VALUES (20, 20, 8);
+INSERT INTO nota (id, resposta_id, nota) VALUES (21, 21, 8);
+INSERT INTO nota (id, resposta_id, nota) VALUES (22, 22, 9);
+INSERT INTO nota (id, resposta_id, nota) VALUES (23, 23, 10);
+INSERT INTO nota (id, resposta_id, nota) VALUES (24, 24, 2);
+INSERT INTO nota (id, resposta_id, nota) VALUES (25, 25, 0);
+INSERT INTO nota (id, resposta_id, nota) VALUES (26, 26, 1);
+INSERT INTO nota (id, resposta_id, nota) VALUES (27, 27, 4);
