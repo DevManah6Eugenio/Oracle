49f3badb5436d6fadc490fd5333e873671a109bd
Manasses Java <manasses.java@gmail.com>
2018-06-27 22:44:36 -0300

    Create 01-aluno.sql

---
+++ projeto03/01-aluno.sql
@@ -0,0 +1,22 @@
+CREATE TABLE aluno (
+  id number primary key,
+  nome varchar2(30) DEFAULT '' NOT NULL,
+  email varchar2(30) DEFAULT '' NOT NULL
+);
+
+INSERT INTO aluno (id, nome, email) VALUES (1, 'Joao da Silva', 'joao@dasilva.com');
+INSERT INTO aluno (id, nome, email) VALUES (2, 'Frederico Jose', 'fred@jose.com');
+INSERT INTO aluno (id, nome, email) VALUES (3, 'Alberto Santos', 'alberto@santos.com');
+INSERT INTO aluno (id, nome, email) VALUES (4, 'Renata Alonso', 'renata@alonso.com');
+INSERT INTO aluno (id, nome, email) VALUES (5, 'Paulo da Silva', 'paulo@dasilva.com');
+INSERT INTO aluno (id, nome, email) VALUES (6, 'Carlos Cunha', 'carlos@cunha.com');
+INSERT INTO aluno (id, nome, email) VALUES (7, 'Paulo Jose', 'paulo@jose.com');
+INSERT INTO aluno (id, nome, email) VALUES (8, 'Manoel Santos', 'manoel@santos.com');
+INSERT INTO aluno (id, nome, email) VALUES (9, 'Renata Ferreira', 'renata@ferreira.com');
+INSERT INTO aluno (id, nome, email) VALUES (10, 'Paula Soares', 'paula@soares.com');
+INSERT INTO aluno (id, nome, email) VALUES (11, 'Jose da Silva', 'jose@dasilva.com');
+INSERT INTO aluno (id, nome, email) VALUES (12, 'Danilo Cunha', 'danilo@cunha.com');
+INSERT INTO aluno (id, nome, email) VALUES (13, 'Zilmira Jose', 'zilmira@jose.com');
+INSERT INTO aluno (id, nome, email) VALUES (14, 'Cristaldo Santos', 'cristaldo@santos.com');
+INSERT INTO aluno (id, nome, email) VALUES (15, 'Osmir Ferreira', 'osmir@ferreira.com');
+INSERT INTO aluno (id, nome, email) VALUES (16, 'Claudio Soares', 'claudio@soares.com');
