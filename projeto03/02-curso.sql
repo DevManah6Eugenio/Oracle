180a454c2a027ca204ce2f7be2b1914f0f64eaac
Manasses Java <manasses.java@gmail.com>
2018-06-27 22:45:45 -0300

    Create 02-curso.sql

---
+++ projeto03/02-curso.sql
@@ -0,0 +1,14 @@
+CREATE TABLE curso (
+  id number primary key,
+  nome varchar2(40) DEFAULT '' NOT NULL
+);
+
+INSERT INTO curso (id, nome) VALUES (1, 'SQL e banco de dados');
+INSERT INTO curso (id, nome) VALUES (2, 'Desenvolvimento web com VRaptor');
+INSERT INTO curso (id, nome) VALUES (3, 'Scrum e metodos ageis');
+INSERT INTO curso (id, nome) VALUES (4, 'C# e orientacao a objetos');
+INSERT INTO curso (id, nome) VALUES (5, 'Java e orientacao a objetos');
+INSERT INTO curso (id, nome) VALUES (6, 'Desenvolvimento mobile com iOS');
+INSERT INTO curso (id, nome) VALUES (7, 'Desenvolvimento mobile com Android');
+INSERT INTO curso (id, nome) VALUES (8, 'Ruby on Rails');
+INSERT INTO curso (id, nome) VALUES (9, 'PHP e MySql');
