a60ea2d92eb39ec82801d9ccf22baf84b011577b
Manasses Java <manasses.java@gmail.com>
2018-06-28 16:55:05 -0300

    Update 08-select.sql

--- projeto03/08-select.sql
+++ projeto03/08-select.sql
@@ -1,12 +1,15 @@
 /*Busque todos os alunos que não tenham nenhuma matrícula nos cursos*/
-select a.nome from aluno a where not exists (select m.id from matricula m where m.aluno_id = a.id);
+select aluno.nome from aluno where not exists (select matricula.id from matricula where matricula.aluno_id = aluno.id);
 
 /*Busque todos os alunos que não tiveram nenhuma matrícula no último ano, usando a instrução EXISTS*/
-select a.nome from aluno a where not exists (select m.id from matricula m where a.id = m.id and M.DATA > (select sysdate - interval '1' year from dual));
+select a.nome from aluno a where not exists (
+    select m.id from matricula m where a.id = m.id and M.DATA > (select sysdate - interval '1' year from dual));
 
-select a.nome from aluno a where (
-    select count(m.id) from matricula m where m.aluno_id = a.id and 
-    m.data > (select sysdate - interval '1' year from dual)) = 0;
-    
 /*Busque todos os alunos que não tiveram nenhuma matrícula no ano de 2015, usando a instrução EXISTS*/
-select a.nome from aluno a join matricula where 
+select a.nome from aluno a where not exists ( select m.id from matricula m 
+    where m.aluno_id = a.id and m.data < '01-01-2015' );
+
+-------------------------------------------------------------------------------------------------------------
+/*Exiba a média das notas por curso.*/
+select c.nome from curso c
+    join nota;
